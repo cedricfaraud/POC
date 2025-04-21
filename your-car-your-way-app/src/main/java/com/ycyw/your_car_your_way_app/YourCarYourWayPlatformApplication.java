19069331aeb9c803fde4131382e9edf59b331357
cedricfaraud <cfaraud@gmail.com>
2025-04-21 22:48:47 +0200

    Initial Commit

---
+++ your-car-your-way-app/src/main/java/com/ycyw/your_car_your_way_app/YourCarYourWayPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ycyw.your_car_your_way_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourCarYourWayPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourCarYourWayPlatformApplication.class, args);
+	}
+
+}
